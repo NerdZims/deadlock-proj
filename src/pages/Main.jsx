101107888d523a7a9fb553ebc6f3f6943b20d5dd
J. Merrill <justin@vrtxdigital.com>
2026-06-21 23:36:47 -0400

    Sideabar stylized, added some styling to body of site.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,6 +6,17 @@ import Footer from "../components/Footer.jsx";
 function Main(){
     return(
         <div className="main-ctr">
+            <aside class="sidebar">
+                <h3>Menu</h3>
+                <hr/>
+                <nav class="nav">
+                <ul>
+                    <li><a href="#">Gun</a></li>
+                    <li><a href="#">Spirit</a></li>
+                    <li><a href="#">Vitality</a></li>
+                </ul>
+                </nav>
+            </aside>
             <div className="main-content-ctr">
                 <h1>Hello Scss</h1>
                 <p>We want to collect the cards in the game and organize them. Cards collected not yet organized<br/>First by category<br/>Followed by tier<br/>And lastly Alphabetically</p>
@@ -16,8 +27,8 @@ function Main(){
                 <div className="item-list">
                     <Items/>
                 </div>
+                <Footer/>
             </div>
-            <Footer/>
         </div>
     )
 }
